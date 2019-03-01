926cf534c13154ba270a6ed98b68cf6f821ffee5
Marissa <m4rsibar@gmail.com>
2019-03-01 16:13:07 -0500

    .

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ class App extends Component {
 
   render() {
     return (
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <div className="App">
           <Header
             randomizeCharacters={this.randomizeCharacters}
