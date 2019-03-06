5c6b836755fedaf84d8845a9a639e306cfb6f50e
Marissa <m4rsibar@gmail.com>
2019-03-05 20:35:29 -0500

    Styling planets/episodes

--- src/Planet.js
+++ src/Planet.js
@@ -4,15 +4,16 @@ const Planet = props => {
   return (
     <div className="planetContainer">
       <h4>{props.p.name}</h4>
+      <hr className="planetDivider" />
       <img
         src="https://img.icons8.com/material/24/000000/planet.png"
         alt="planet"
         className="planetIcon"
       />
       <div>
-        <span>Type:{props.p.type}</span>
+        <span>Type: {props.p.type}</span>
         <br />
-        <span>Dimension:{props.p.dimension}</span>
+        <span>Dimension: {props.p.dimension}</span>
         <br />
         <span>Created: {props.p.created} </span>
       </div>
