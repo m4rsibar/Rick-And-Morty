e37d6ae1ca4603c4eaac8a5354964c36101e4188
Marissa <m4rsibar@gmail.com>
2019-03-05 20:51:51 -0500

    Temporarily switched back to BrowserRouter for animaton

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { HashRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import CharacterList from "./CharacterList";
 import Header from "./Header";
 import PlanetsList from "./PlanetsList";
@@ -8,6 +8,7 @@ import EpisodeList from "./EpisodeList";
 import Search from "./Search";
 import Loading from "./Loading";
 import OutsideAlerter from "./OutsideAlerter";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 class App extends Component {
   state = {
@@ -107,7 +108,7 @@ class App extends Component {
 
   render() {
     return (
-      <HashRouter>
+      <BrowserRouter>
         <div className="App">
           <OutsideAlerter
             menuOpen={this.state.menuOpen}
@@ -139,76 +140,88 @@ class App extends Component {
             </span>
           )}
 
-          <Switch>
-            <Route
-              exact
-              path="/"
-              render={() =>
-                this.state.isLoading ? (
-                  <Loading />
-                ) : (
-                  <CharacterList
-                    characters={this.state.characters}
-                    search={this.state.search}
-                    nextCharacter={this.state.charactersNext}
-                    prevCharacter={this.state.charactersPrev}
-                    fetchSomeData={this.fetchSomeData}
-                    scroll={this.scroll}
-                  />
-                )
-              }
-            />
-            <Route
-              path="/planets"
-              render={() =>
-                this.state.isLoading ? (
-                  <Loading />
-                ) : (
-                  <PlanetsList
-                    planets={this.state.planets}
-                    nextPlanet={this.state.planetsNext}
-                    prevPlanet={this.state.planetsPrev}
-                    fetchSomeData={this.fetchSomeData}
-                    toggleMenu={this.toggleMenu}
-                    scroll={this.scroll}
-                  />
-                )
-              }
-            />
-            <Route
-              path="/episodes"
-              render={() =>
-                this.state.isLoading ? (
-                  <Loading />
-                ) : (
-                  <EpisodeList
-                    episodes={this.state.episodes}
-                    nextEpisode={this.state.episodesNext}
-                    prevEpisode={this.state.episodesPrev}
-                    fetchSomeData={this.fetchSomeData}
-                    toggleMenu={this.toggleMenu}
-                    scroll={this.scroll}
-                  />
-                )
-              }
-            />
-            <Route
-              path="/search"
-              render={() =>
-                this.state.isLoading ? (
-                  <Loading />
-                ) : (
-                  <Search
-                    characters={this.state.allCharacters}
-                    handleSubmit={this.handleSubmit}
-                  />
-                )
-              }
-            />
-            <Route component={NotFound} />
-          </Switch>
+          <Route
+            render={({ location }) => (
+              <TransitionGroup>
+                <CSSTransition
+                  key={location.key}
+                  timeout={300}
+                  classNames="fade"
+                >
+                  <Switch>
+                    <Route
+                      exact
+                      path="/"
+                      render={() =>
+                        this.state.isLoading ? (
+                          <Loading />
+                        ) : (
+                          <CharacterList
+                            characters={this.state.characters}
+                            search={this.state.search}
+                            nextCharacter={this.state.charactersNext}
+                            prevCharacter={this.state.charactersPrev}
+                            fetchSomeData={this.fetchSomeData}
+                            scroll={this.scroll}
+                          />
+                        )
+                      }
+                    />
+                    <Route
+                      path="/planets"
+                      render={() =>
+                        this.state.isLoading ? (
+                          <Loading />
+                        ) : (
+                          <PlanetsList
+                            planets={this.state.planets}
+                            nextPlanet={this.state.planetsNext}
+                            prevPlanet={this.state.planetsPrev}
+                            fetchSomeData={this.fetchSomeData}
+                            toggleMenu={this.toggleMenu}
+                            scroll={this.scroll}
+                          />
+                        )
+                      }
+                    />
+                    <Route
+                      path="/episodes"
+                      render={() =>
+                        this.state.isLoading ? (
+                          <Loading />
+                        ) : (
+                          <EpisodeList
+                            episodes={this.state.episodes}
+                            nextEpisode={this.state.episodesNext}
+                            prevEpisode={this.state.episodesPrev}
+                            fetchSomeData={this.fetchSomeData}
+                            toggleMenu={this.toggleMenu}
+                            scroll={this.scroll}
+                          />
+                        )
+                      }
+                    />
+                    <Route
+                      path="/search"
+                      render={() =>
+                        this.state.isLoading ? (
+                          <Loading />
+                        ) : (
+                          <Search
+                            characters={this.state.allCharacters}
+                            handleSubmit={this.handleSubmit}
+                          />
+                        )
+                      }
+                    />
+                    <Route component={NotFound} />
+                  </Switch>
+                </CSSTransition>
+              </TransitionGroup>
+            )}
+          />
         </div>
-      </HashRouter>
+      </BrowserRouter>
     );
   }
 }
